27a3c888f944186ccded3b67938e590685939194
AnhQuân <105381094+quanda21@users.noreply.github.com>
2023-07-23 02:33:21 +0700

    update
    update list

--- HE153620_Store/HE153620_Store/Pages/Admin/Product/List.cshtml.cs
+++ HE153620_Store/HE153620_Store/Pages/Admin/Product/List.cshtml.cs
@@ -1,4 +1,4 @@
-using HE153620_Store.Models;
+﻿using HE153620_Store.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,18 +6,25 @@ namespace HE153620_Store.Pages.Admin.Product
 {
     public class ListModel : PageModel
     {
-        private readonly StoreManagerContext _dbContext;
+       
+            private readonly StoreManagerContext _dbContext;
 
-        public ListModel(StoreManagerContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+            public ListModel(StoreManagerContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
 
-        public List<Pages_Admin_Product_List> Products { get; set; }
+            public List<Models.Product> Products { get; set; } // Sửa lại kiểu dữ liệu
 
-        public void OnGet()
-        {
-            Products = new List<Pages_Admin_Product_List>();   
-        }
+            public List<Supplier> Suppliers { get; set; }
+
+            public List<Category> Categories { get; set; }
+
+            public void OnGet()
+            {
+               Suppliers=_dbContext.Suppliers.ToList();
+                Products = _dbContext.Products.ToList();
+            Categories = _dbContext.Categories.ToList();    
+            }
     }
 }
